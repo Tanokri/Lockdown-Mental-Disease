5df1876fcf0271204c9ad513e624b8b856f54616
Tanokri <tanay.p.sanghvi@gmail.com>
2021-12-25 03:14:05 +0530

    2048 Colour Coding

---
+++ 2048_Colour.py
@@ -0,0 +1,48 @@
+size = 1080
+length = 4
+thickness = 15
+back = "#000000"
+cells = "#2d2d2e"
+white = "#ffffff"
+cellmap = {
+2:      "#03b1fc",
+4:      "#0373fc",
+8:      "#4137d4",
+16:     "#7637d4",
+32:     "#a537d4",
+64:     "#cf37d4",
+128:    "#e80083",
+256:    "#e80055",
+512:    "#e80017",
+1024:   "#00e8b2",
+2048:   "#00dce8",
+4096:   "#018087",
+8192:   "#005e63",
+16384:  "#01494d",
+32768:  "#013336",
+65536:  "#011617",
+}
+cellfont = {
+2:      "#ffffff",
+4:      "#ffffff",
+8:      "#ffffff",
+16:     "#ffffff",
+32:     "#ffffff",
+64:     "#ffffff",
+128:    "#ffffff",
+256:    "#ffffff",
+512:    "#ffffff",
+1024:   "#ffffff",
+2048:   "#ffffff",
+4096:   "#ffffff",
+8192:   "#ffffff",
+16384:  "#ffffff",
+32768:  "#ffffff",
+65536:  "#ffffff",
+}
+font = ("Bodoni",36,"bold")
+up = "up"
+down = "down"
+left = "left"
+right = "right"
+
